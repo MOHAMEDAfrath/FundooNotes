6a60b01772a682ca682b7b2e7d7d513ebf78f920
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-29 12:56:55 +0530

    [Afrath]Added:- Password Encryption

--- Repository/Repository/UserRepository.cs
+++ Repository/Repository/UserRepository.cs
@@ -4,6 +4,7 @@ using Repository.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace FundooNotes.Repository.Repository
@@ -21,6 +22,7 @@ namespace FundooNotes.Repository.Repository
             try{
                 if (userData != null)
                 {
+                    userData.Password = encryptPassword(userData.Password);
                     this.userContext.Users.Add(userData);
                     this.userContext.SaveChanges();
                     return true;
@@ -31,6 +33,17 @@ namespace FundooNotes.Repository.Repository
             {
                 throw new Exception(ex.Message);
             }
+        }
+        /// <summary>
+        /// Encryption using Base64
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public string encryptPassword(string password)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(password);
+            string encodedText = Convert.ToBase64String(plainTextBytes);
+            return encodedText;
         }
     }
 }
